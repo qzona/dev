09ae7b76ba7b76c747746635dfaf2292ce4e579d
Quinn Zona <qzona@bsd220.org>
2021-11-08 10:44:43 -0600

    added dice images

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -14,8 +14,9 @@ struct ContentView: View {
             Text("Dice Roll")
                 .font(.title)
                 .padding()
-            Text("\(randomValue)")
-                .font(.system(size: 72))
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .padding()
                 .onTapGesture {
                     randomValue = Int.random(in: 1...6)
